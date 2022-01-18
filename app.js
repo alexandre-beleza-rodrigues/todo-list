949bd4193c576a7bb1751da4c6c4a1b479659bec
Alexandre Rodrigues <alexandrebelezarodrigues@gmail.com>
2022-01-18 15:51:19 +0000

    completed todo list without storage

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo')
 
 
-todoButton.addEventListener('click', addTodo)
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 
 function addTodo(event) {
@@ -31,4 +34,44 @@ function addTodo(event) {
     todoList.appendChild(todoDiv);
 
     todoInput.value = "";
+};
+
+function deleteCheck(e) {
+    const item = e.target;
+
+    if (item.classList[0] === "trash-btn") {
+        const todo = item.parentElement;
+        todo.classList.add('fall')
+        todo.addEventListener("transitionend", function() {
+            todo.remove();    
+        });
+    }
+    if (item.classList[0] === "completed-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+};
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo) {
+        switch (e.target.value) {
+        case "all":
+                todo.style.display = "flex";
+            break;
+        case "completed":
+            if (todo.classList.contains("completed")) {
+                todo.style.display = "flex";
+            } else {
+                todo.style.display = "none";
+            }
+            break;
+        case "uncompleted":
+            if (!todo.classList.contains("completed")) {
+                todo.style.display = "flex";
+            } else {
+                todo.style.display = "none";
+            }
+        }
+    });
 }
